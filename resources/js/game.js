9e9f4c1de3fb1331c1d9110abbef6ba75d973f65
Jose D. Castellon <jd.castellonm@gmail.com>
2018-08-26 00:04:12 -0400

    add collision, implement field as array

--- resources/js/game.js
+++ resources/js/game.js
@@ -11,6 +11,7 @@ const matrix = [
 ];
 
 // create playing field
+// the field is represented by a 2D array where 0's are empty spaces and non-zeros are occupied
 function createField(width, height) {
     const fieldMatrix = [];
     while (height--) {
@@ -19,9 +20,39 @@ function createField(width, height) {
     return fieldMatrix;
 }
 
+const field = createField(12, 20);
+
 const player = {
-    position: {x: 5, y: 5},
+    position: {x: 5, y: 0},
     matrix: matrix
+}
+
+// populate the 2D array field with non-zero values which represent the spaces
+// where the player has their pieces
+function spawn_on_field(field, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                field[y + player.position.y][x + player.position.x] = value;
+            }
+        })
+    })
+}
+
+// use the non-zero values on the 2D array of the field as a way to check for
+// 'collision'. pieces can move freely in any '0' spaces
+function collision(field, player) {
+    let [piece, offset] = [player.matrix, player.position];
+    for (let row = 0; row < piece.length; ++row) {
+        for (let col = 0; col < piece[row].length; ++col) {
+            if (piece[row][col] !== 0 &&
+                (field[row + offset.y] && // check if a row exists/is available
+                field[row + offset.y][col + offset.x]) !== 0) { // check if the specific tile is occupied
+                    return true; // space is occupied, return true
+                }
+        }
+    }
+    return false;
 }
 
 // function to draw a piece to the canvas
@@ -41,7 +72,8 @@ function drawPiece(matrix, offset) {
 function draw() {
     ctx.fillStyle = '#000';
     ctx.fillRect(0,0,canvas.width, canvas.height);
-    drawPiece(player.matrix, player.position);
+    drawPiece(field, {x: 0, y: 0}); // draw the pieces on the field
+    drawPiece(player.matrix, player.position); // draw the current piece in play
 }
 
 // piece movement events
@@ -59,6 +91,11 @@ document.addEventListener('keydown', event => {
 
 function softDrop() {
     player.position.y++;
+    if (collision(field, player))  { // if theres collision, move the piece back up by 1, so that they don't 'fuse'
+        player.position.y--;
+        spawn_on_field(field, player);
+        player.position.y = 0; // reset player position back to top
+    }
     elapsedMilliseconds = 0;
 }
 
